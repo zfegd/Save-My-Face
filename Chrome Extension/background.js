1daeb334fa07cc8447bb611c9b85eb8c9eb6d4fa
Ash <ashley.see16@imperial.ac.uk>
2017-04-01 13:28:48 +0100

    todo querying next

--- Chrome Extension/background.js
+++ Chrome Extension/background.js
@@ -1,8 +1,20 @@
 function sendToCheck(info,tab) {
   console.log("Word " + info.selectionText + " was clicked.");
+  var str = info.selectionText;
+  var json = JSON.stringify(str);
   chrome.tabs.create({  
-    url: "http://www.google.com/search?q=" + info.selectionText,
-  });           
+    url: "popupwindow.html",
+  }, function(tab) {
+     chrome.windows.create({
+        tabId: tab.id,
+        type: 'popup',
+        focused: true,
+        width: 240,
+        height: 240,
+        top: 320,
+        left: 640
+    }); 
+  });
 }
 
 chrome.contextMenus.create({
